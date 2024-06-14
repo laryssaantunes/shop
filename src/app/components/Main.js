57e46221647de463a29e40b86fef0ba69b531b99
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-06-14 16:08:52 -0400

    commit 14/06/2024
    Spinner

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./main.module.css";
 import Image from "next/image";
+import Spinner from "./Spinner";
+
+
 
 export default async function Main() {
     const response = await fetch ("https://fakestoreapi.com/products");
@@ -25,6 +28,11 @@ export default async function Main() {
                 </div>
             ))}
             </div>
+
+
+            if(listProduct[0] == null){
+                return <Spinner/>
+            };
         </main>
     );
 }
---
+++ src/app/components/Spinner.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+export default function Spinner() {
+    return(
+        <Image
+        widht={100}
+        heigth={100}
+        scr="12-dots-scale-rotate.svg"
+        all=""
+        ></Image>
+    );
+}
